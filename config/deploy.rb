bfa3d16c0766f13861a139220b4bc96d39db033d
oducroux <ducrouxolivier@gmail.com>
2017-03-18 22:21:33 +0100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.0"
 
 # Change these
-server '95.85.6.65', port: 80, roles: [:web, :app, :db], primary: true
+server '95.85.6.65', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:oducroux/rouvignac.git'
 set :application,     'rouvignac'
@@ -21,7 +21,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
