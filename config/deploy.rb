1ca37afb79d47de908d5d6a4e1c3d5be5db2abfe
oducroux <ducrouxolivier@gmail.com>
2017-03-19 16:25:36 +0100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -104,6 +104,8 @@ namespace :deploy do
   after  :finishing,    :restart
 end
 
+after 'deploy', 'puma:restart'
+
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
