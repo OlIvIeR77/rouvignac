688d9d279d48e5e3d02a3f0998cf619f761a492b
oducroux <ducrouxolivier@gmail.com>
2017-03-19 01:31:52 +0100

    deploy

--- app/models/gite.rb
+++ app/models/gite.rb
@@ -1,6 +1,6 @@
 class Gite < ApplicationRecord
   #attr_accessible :text1, :text2, :text3, :text4, :text5, :text6, :text7, :text8, :text9, :text10, :title, :image
-
+  default_scope { where.not(title: "Prochainement") }
   #def to_param
   #  "#{id} #{title}".parameterize
   #end
