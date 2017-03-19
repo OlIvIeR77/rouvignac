dc5f673fc6f8436eb24db72561283536515ec1dd
oducroux <ducrouxolivier@gmail.com>
2017-03-19 11:46:56 +0100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,30 @@
 # config valid only for current version of Capistrano
 lock "3.8.0"
 
-# Change these
 server '95.85.6.65', port: 22, roles: [:web, :app, :db], primary: true
-
 set :repo_url,        'git@github.com:oducroux/rouvignac.git'
+set :branch,          'master'
 set :application,     'rouvignac'
 set :user,            'deployer'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+
+#set :puma_threads,    [4, 16]
+#set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+#set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+#set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+#set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+#set :puma_access_log, "#{release_path}/log/puma.error.log"
+#set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+#set :puma_preload_app, true
+#set :puma_worker_timeout, nil
+#set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -64,19 +64,19 @@ namespace :deploy do
     end
   end
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
+  #desc 'Initial Deploy'
+  #task :initial do
+  #  on roles(:app) do
+  #    before 'deploy:restart', 'puma:start'
+  #    invoke 'deploy'
+  #  end
+  #end
+  #desc 'Restart application'
+  #task :restart do
+  #  on roles(:app), in: :sequence, wait: 5 do
+  #    invoke 'puma:restart'
+  #  end
+  #end
 
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
