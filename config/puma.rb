faeaa2b3a811b07a6d8beb485a8fbeeba7407f15
oducroux <ducrouxolivier@gmail.com>
2017-03-19 12:47:01 +0100

    deploy

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,7 @@ threads 8, 16
 daemonize true
 bind "unix:///home/deployer/apps/rouvignac/shared/tmp/sockets/rouvignac-puma.sock"
 pidfile "/home/deployer/apps/rouvignac/shared/tmp/pids/puma.pid"
-
+environment "production"
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
