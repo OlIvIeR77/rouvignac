f54b525f812bad7a39e3fbc77742b25fbb3671e5
dm0lz <ducrouxolivier@gmail.com>
2017-04-25 20:41:25 +0200

    beds migration

--- db/migrate/20170425153234_add_beds_to_gites.rb
+++ db/migrate/20170425153234_add_beds_to_gites.rb
@@ -1,5 +1,5 @@
 class AddBedsToGites < ActiveRecord::Migration[5.0]
   def change
-    add_column :gites, :beds, :string, array: true, default: [].to_yaml
+    add_column :gites, :beds, :string, array: true, default: []
   end
 end
