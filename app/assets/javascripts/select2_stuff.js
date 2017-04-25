4822aa892015425d86c4e563426b27695dff6102
dm0lz <ducrouxolivier@gmail.com>
2017-04-25 20:18:55 +0200

    gites form

--- app/assets/javascripts/select2_stuff.js
+++ app/assets/javascripts/select2_stuff.js
@@ -2,4 +2,6 @@
 $(document).ready(function(){
   $("#reservation_client_id").select2({ width: '97%' });
   $("#reservation_gite_id").select2({ width: '97%' });
+  $("#gite_text1").select2({ width: '97%' });
+  $("#gite_beds").select2({ width: '97%' });
 });
